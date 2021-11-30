963e44494194848e1bdee31e19d878b0e0b9cca6
Lukas <74739057+Luka5W@users.noreply.github.com>
2021-11-30 23:19:40 +0100

    Update JSUtils.js

--- JSUtils.js
+++ JSUtils.js
@@ -1,2 +1,6 @@
-// Returns the first key with the given value
-if (!Object.prototype.getKeyByValue) Object.prototype.getKeyByValue = function(value) { return Object.keys(this).find(k => this[k] === value ); };
+module.exports = {
+    object: {
+        // Returns the first key with the given value
+        getKeyByValue: (object, value) => Object.keys(object).find(k => this[k] === value )
+    }
+}
