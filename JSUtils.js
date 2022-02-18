3e09aee7a8f77fa12b986f5b40e25222b37eb6c9
Lukas <74739057+Luka5W@users.noreply.github.com>
2022-02-18 18:18:25 +0100

    Update JSUtils.js

--- JSUtils.js
+++ JSUtils.js
@@ -1,6 +1,6 @@
 module.exports = {
     object: {
         // Returns the first key with the given value
-        getKeyByValue: (object, value) => Object.keys(object).find(k => this[k] === value )
+        getKeyByValue: (object, value) => Object.keys(object).find(k => object[k] === value )
     }
 }
